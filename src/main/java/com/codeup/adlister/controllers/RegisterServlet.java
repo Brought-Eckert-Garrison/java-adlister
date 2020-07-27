4aaccd7db602a1ed8ead671a4482fabb044af780
Andrew Brought <andrewbrought@gmail.com>
2020-07-27 12:54:42 -0500

    debugging register servlet

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -2,12 +2,14 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
@@ -17,25 +19,46 @@ public class RegisterServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("reg_username");
+        String username = request.getParameter("username");
         String email = request.getParameter("email");
-        String password = request.getParameter("reg_password");
+        String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
+        User duplicate = DaoFactory.getUsersDao().findByUsername(username);
+        String hashedPw = BCrypt.hashpw(password, BCrypt.gensalt(12));
+//        String message = null;
+
 
         // validate input
         boolean inputHasErrors = username.isEmpty()
-            || email.isEmpty()
-            || password.isEmpty()
-            || (! password.equals(passwordConfirmation));
+                || email.isEmpty()
+                || password.isEmpty();
+
+//        boolean passwordsNotValid = !BCrypt.checkpw(passwordConfirmation, hashedPw);
+
 
         if (inputHasErrors) {
+//            message = "Not all fields were filled in";
+//            request.setAttribute("message", message);
             response.sendRedirect("/register");
-            return;
+            JOptionPane.showMessageDialog(null, "Not all fields were properly filled in. Please try again! : )");
+        } //else if(passwordsNotValid){
+//            response.sendRedirect("/register");
+//            JOptionPane.showMessageDialog(null, "Passwords did not match. Please re-enter your passwords! : )");
+//        }
+        else if(duplicate != null) {
+//            message = "This user already exists";
+//            request.setAttribute("message", message);
+            response.sendRedirect("/register");
+            JOptionPane.showMessageDialog(null, "This username already exists. Please enter something original! : )");
+        } else {
+//            message = " ";
+//            request.setAttribute("message", message);
+
+            User user = new User(username, email, hashedPw);
+            DaoFactory.getUsersDao().insert(user);
+            response.sendRedirect("/login");
         }
 
-        // create and save a new user
-        User user = new User(username, email, password);
-        DaoFactory.getUsersDao().insert(user);
-        response.sendRedirect("/login");
+
     }
 }
